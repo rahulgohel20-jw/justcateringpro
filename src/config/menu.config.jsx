5c163b11f9a4121adb54aac5854117fe11a35def
rahulgohel20-jw <rahul.shree0101@gmail.com>
2026-09-09 19:50:58 +0530

    change a button in purchase approval screen

--- src/config/menu.config.jsx
+++ src/config/menu.config.jsx
@@ -1079,6 +1079,51 @@ id="COMMON.ITEMRAWMATERIALUNITCHANGE"              defaultMessage="Item Raw Mate
       ],
     },
 
+
+     {
+        title: (
+          <div className="w-full flex justify-between items-center">
+            <FormattedMessage
+              id="COMMON.STOCK_MANAGEMENT"
+              defaultMessage="Purchase Approve"
+            />
+          </div>
+        ),
+        icon: "ki-filled ki-verify text-primary text-lg",
+        
+        moduleName: "Purchase Approve",
+        disabled: false,
+        children: [
+          {
+            title: (
+              <FormattedMessage
+                id="COMMON.RAW_MATERIAL_OPB"
+                defaultMessage="Purchase Approve Request"
+              />
+            ),
+            path: "/purchase-return-request",
+            pageName: "Purchase Approve Request",
+          },
+  {
+            title: (
+              <FormattedMessage
+                id="COMMON.RAW_MATERIAL_OPB"
+                defaultMessage="Purchase Approve "
+              />
+            ),
+            path: "/purchase/approval-request-List",
+            pageName: "Purchase Approve",
+          },
+          
+        
+        
+        
+        
+
+          
+        ],
+      },
+
     {
       title: (
         <div className="w-full flex justify-between items-center">
@@ -1248,49 +1293,7 @@ id="COMMON.ITEMRAWMATERIALUNITCHANGE"              defaultMessage="Item Raw Mate
     },
 
     
-  {
-        title: (
-          <div className="w-full flex justify-between items-center">
-            <FormattedMessage
-              id="COMMON.STOCK_MANAGEMENT"
-              defaultMessage="Purchase Approve"
-            />
-          </div>
-        ),
-        icon: "ki-filled ki-chart-simple text-primary text-lg",
-        
-        moduleName: "Purchase Approve",
-        disabled: false,
-        children: [
-          {
-            title: (
-              <FormattedMessage
-                id="COMMON.RAW_MATERIAL_OPB"
-                defaultMessage="Purchase Approve Request"
-              />
-            ),
-            path: "/purchase-return-request",
-            pageName: "Purchase Approve Request",
-          },
-  {
-            title: (
-              <FormattedMessage
-                id="COMMON.RAW_MATERIAL_OPB"
-                defaultMessage="Purchase Approve "
-              />
-            ),
-            path: "/purchase/approval-request-List",
-            pageName: "Purchase Approve",
-          },
-          
-        
-        
-        
-        
-
-          
-        ],
-      },
+ 
 	
 
     // {
